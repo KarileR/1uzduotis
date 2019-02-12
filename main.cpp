432cebc3b884e9e6789548fce33c82798cbd9f94
KarileR <44968273+KarileR@users.noreply.github.com>
2019-02-12 05:57:59 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,14 +7,22 @@ int main()
 
     std::cout<<"Iveskite savo varda: "<<std::endl;
     std::cin>>vardas;
-    
-    std::string TreciaEil = "* Sveikas, " + vardas + "! *";
+
+    std::string TreciaEil;
+
+    if (vardas.back() == 's')
+    {
+        TreciaEil = "* Sveikas, " + vardas + "! *";
+    }
+    else
+    {
+        TreciaEil = "* Sveika, " + vardas + "! *";
+    }
 
     std::string tarpas(TreciaEil.size() - 4, ' ');
     std::string AntraEil = "* " + tarpas + " *";
 
     std::string PirmaEil(AntraEil.size(), '*');
-
 
     std::cout<<PirmaEil<<std::endl;
     std::cout<<AntraEil<<std::endl;
